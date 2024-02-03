cbd63720a59431baabeb1238b9d1700cdd78af95
PoPodada <goito.722@icloud.com>
2024-02-03 18:40:39 +0900

    a

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,23 @@
-import { useState } from "react";
+import { useState,useCallback,useMemo } from "react";
 import "./styles.css";
 import { ChildArea } from "./ChildArea";
 export const App = () => {
-  const [count,setCount] = useState(0);
+  const [text,setText] = useState("");
+  const [open,setOpen] = useState(false);
   // const onClickCountUp = () =>{
   //   setCount(count+1)
   // }
+  const onClickOpen = () =>setOpen(!open)
+  const onChangeText = (e) => setText(e.target.value) 
+  const onClickClose = useCallback(() => setOpen(false),[]);
+ 
   return (
     <div className="App">
-      <input />
+      <input value={text} onChange={onChangeText}/>
       <br />
       <br />
-      <button>表示</button>
-      <ChildArea/>
+      <button onClick={onClickOpen}>表示</button>
+      <ChildArea open = {open} onClickClose = {onClickClose}/>
     </div>
   );
 }
