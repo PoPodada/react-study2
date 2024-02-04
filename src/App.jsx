860607c7a3659de31d069978dfd38e97718a96a1
go <goito.722@icloud.com>
2024-02-04 11:00:14 +0900

    a

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,58 @@
-import { useState,useCallback,useMemo } from "react";
+import { useState,useCallback,useMemo,useRef} from "react";
 import "./styles.css";
 import { ChildArea } from "./ChildArea";
 export const App = () => {
-  const [text,setText] = useState("");
-  const [open,setOpen] = useState(false);
+  const [email,setEmail] = useState("");
+  const [password,setPassword] = useState("");
+
+  const emailRef = useRef(null);
+  const passwordRef = useRef(null);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log({
+      emai: emailRef.current.value,
+      password: passwordRef.current.value,
+    });
+  };
+
+  return (
+    <div className="App">
+      <h1>ログイン</h1>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="email">Email</label>
+          <input id="email" name="email" ref={emailRef} />
+        </div>
+        <div>
+          <label htmlFor="password">パスワード</label>
+          <input id="password" name="password" ref={passwordRef} type="password" />
+        </div>
+
+        <div>
+          <button type="submit">ログイン</button>
+        </div>
+      </form>
+    </div>
+  );
+  // const [text,setText] = useState("");
+  // const [open,setOpen] = useState(false);
   // const onClickCountUp = () =>{
   //   setCount(count+1)
   // }
-  const onClickOpen = () =>setOpen(!open)
-  const onChangeText = (e) => setText(e.target.value) 
-  const onClickClose = useCallback(() => setOpen(false),[]);
+  // const onClickOpen = () =>setOpen(!open)
+  // const onChangeText = (e) => setText(e.target.value) 
+  // const onClickClose = useCallback(() => setOpen(false),[]);
  
-  return (
-    <div className="App">
-      <input value={text} onChange={onChangeText}/>
-      <br />
-      <br />
-      <button onClick={onClickOpen}>表示</button>
-      <ChildArea open = {open} onClickClose = {onClickClose}/>
-    </div>
-  );
+  // return (
+  //   <div className="App">
+  //     <input value={text} onChange={onChangeText}/>
+  //     <br />
+  //     <br />
+  //     <button onClick={onClickOpen}>表示</button>
+  //     <ChildArea open = {open} onClickClose = {onClickClose}/>
+  //   </div>
+  // );
 }
 
 
